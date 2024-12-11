b7870d58dcaec5e0b8e77c35bea337c719dd63f2
Vikas Sanwal <vikassnwl@gmail.com>
2024-12-11 10:38:11 +0530

    Add CIFAR10/

---
+++ CIFAR10/model_architecture1.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+def create_model_1():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(32, 32, 1)),
+        tf.keras.layers.Conv2D(96, 3, padding="same", activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Conv2D(96, 3, padding="same", activation="relu"),
+        tf.keras.layers.Conv2D(96, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPool2D(3, 2),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Conv2D(192, 3, padding="same", activation="relu"),
+        tf.keras.layers.Conv2D(192, 1, padding="valid", activation="relu"),
+        tf.keras.layers.Conv2D(10, 1, padding="valid"),
+        tf.keras.layers.GlobalAvgPool2D(),
+        tf.keras.layers.Activation("softmax")
+    ])
+    
+    return model
