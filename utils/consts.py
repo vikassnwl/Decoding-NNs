c495a608121407c0a67333986c818b23c808ef0c
Vikas Sanwal <vikassnwl@gmail.com>
2024-12-14 16:51:38 +0530

    Add utils/consts.py

---
+++ utils/consts.py
@@ -0,0 +1,1 @@
+MODEL_DIR = "/home/vikas/Lab/Decoding NNs/models"
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,10 @@
 import random
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
+import os
+import gdown
 
 
 cifar10_classes = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
@@ -125,3 +129,58 @@ def random_translate(image, width_factor, height_factor):
     )
 
     return tf.squeeze(translated_image)
+
+
+def grid_plot(X, y=None, y_preds=None, class_names=None, scaling_factor=2.5, total_items_to_show=25, seed=None):
+    random.seed(seed)
+    if y is not None and y.shape[1] > 1:
+        y = y.argmax(axis=1)
+
+    if y_preds is not None and y_preds.shape[1] > 1:
+        y_preds = y_preds.argmax(axis=1)
+
+    if isinstance(X, str):
+        directory_path = X
+        directory_items = os.listdir(directory_path)
+        total_items_to_show = min(len(directory_items), total_items_to_show)
+        rand_indices = random.sample(range(len(directory_items)), total_items_to_show)
+        X = []
+        # for item_name in directory_items[:total_items_to_show]:
+        for rand_idx in rand_indices:
+            # item_path = f"{directory_path}/{item_name}"
+            item_path = f"{directory_path}/{directory_items[rand_idx]}"
+            image = cv2.imread(item_path)[..., ::-1]
+            X.append(image)                                          
+    elif not isinstance(X, (list, np.ndarray)):
+        raise(Exception("Either provide an array of images or \
+                        a path to a directory containing images as the first argument."))
+
+    # X = X[:total_items_to_show]
+    total_items_to_show = min(len(X), total_items_to_show)
+    rand_indices = random.sample(range(len(X)), total_items_to_show)
+    cols = int(np.ceil(np.sqrt(total_items_to_show)))
+    rows = int(np.ceil(total_items_to_show/cols))
+    fig, axes = plt.subplots(rows, cols, figsize=(scaling_factor*cols, scaling_factor*rows))
+    for i, rand_idx in enumerate(rand_indices):
+        ax = axes[i//cols][i%cols]
+        image = X[rand_idx]
+        ax.imshow(image)
+        if y is not None:
+            if y_preds is not None:
+                if y_preds[rand_idx].item() == y[rand_idx].item():
+                    ax.set_title(class_names[y[rand_idx].item()], color="green")
+                else:
+                    ax.set_title(class_names[y_preds[rand_idx].item()], color="red")
+                    ax.text(0, 2, class_names[y[rand_idx].item()], color='white', bbox=dict(facecolor='green'))
+            else:
+                ax.set_title(class_names[y[rand_idx].item()])
+        ax.axis("off")
+
+
+def download_model(model_url, model_path):
+    FILE_ID = model_url.split("/")[-2]
+    download_url = f"https://drive.google.com/uc?id={FILE_ID}&export=download"
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    if not os.path.exists(model_path):
+        gdown.download(download_url, model_path)
+    print(f"Model Downloaded Successfully to {model_path}!")
